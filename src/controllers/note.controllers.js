4ed4afe2769fd46c8caf84d4c4d982e14efb8057
Kalpataru Behera <enigmatic.coder.075@gmail.com>
2025-10-26 23:55:26 +0530

    Added notes controllers

---
+++ src/controllers/note.controllers.js
@@ -0,0 +1,124 @@
+import mongoose from "mongoose";
+import {ProjectNote} from "../models/note.models";
+import { asyncHandler } from "../utils/async-handler";
+import { ApiResponse } from "../utils/api-response";
+import { ApiError } from "../utils/api-error";
+import { Project } from "../models/project.models";
+
+const getNotes = asyncHandler(async (req, res) => {
+    const {projectId} = req.params;
+
+    const project = await Project.findById(projectId);
+
+    if(!project) {
+        throw new ApiError(404,"Project not found");
+    }
+
+    const notes = await ProjectNote.find({
+        project: new mongoose.Types.ObjectId(projectId),
+    }).populate("createdBy", "username fullname avatar");
+
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,notes, "notes fetched successfullly"))
+})
+
+const createNote = asyncHandler(async(req, res) => {
+    const {projectId} = req.params;
+    const {content} = req.body;
+
+    const project = await Project.findById(projectId);
+
+    if(!project) {
+        throw new ApiError(404,"Project not found");
+    }
+    const note = await ProjectNote.create({
+        project: new mongoose.Types.ObjectId(project),
+        content,
+        createdBy: new mongoose.Types.ObjectId(req.user._id),
+    })
+
+    if(!note) {
+        throw new ApiError(400,"note not created");
+    }
+
+    const populatedNote = await ProjectNote.findById(note._id)
+    .populate(
+        "createdBy",
+        "username fullName avatar",
+    )
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,populatedNote,"Note created successfully"));
+
+})
+
+
+// deleteNote
+
+const deleteNote = asyncHandler(async(req, res) => {
+    const {noteId} = req.params;
+
+    const note = ProjectNote.findByIdAndDelete(noteId);
+    if(!note) {
+        throw new ApiError(404,"Note not found");
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,note,"Note deleted successfully"));
+    
+})
+
+//updateNote
+
+const updateNote = asyncHandler(async(req, res) => {
+    const {noteId} = req.params;
+    const {content} = req.body;
+
+    const existingNote = await ProjectNote.findById(noteId);
+    if(!existingNote)  {
+        throw new ApiError(404,"Note not found");
+    }
+
+    const note = await ProjectNote.findByIdAndUpdate(
+        noteId,
+        {content},
+        {new: true},
+    ).populate("createdBy", "username fullName avatar");
+
+    if(note) {
+        throw new ApiError(400,"Note can't be updated");
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,note,"Note updated successfully"));
+})
+
+// getNoteById
+
+const getNoteById = asyncHandler(async(req, res) => {
+    const {noteId} = req.params;
+
+    const note = await ProjectNote.findById(noteId).populate("createBy", "username fullName avatar");
+
+    if(!note) {
+        throw new ApiError(404, "Note not found");
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,note,"Note fetched successfully"));
+})
+
+
+export {
+    getNoteById,
+    getNotes,
+    createNote,
+    updateNote,
+    deleteNote,
+};
